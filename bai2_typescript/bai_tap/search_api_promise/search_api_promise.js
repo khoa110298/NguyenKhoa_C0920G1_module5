fdbf154219ff6b6abf3400776e3a21df8157562e
khoa110298 <nguyenkhoa15011998@gmail.com>
2021-02-05 10:18:24 +0700

    upbai

---
+++ bai2_typescript/bai_tap/search_api_promise/search_api_promise.js
@@ -0,0 +1,27 @@
+function searchInput() {
+    let keyword = document.getElementById("keyword").value;
+    let promiseObj = new Promise((resolve, reject) => {
+        let xHttp = new XMLHttpRequest();
+        xHttp.onload = function () {
+            if (this.status == 200) {
+                resolve(xHttp.responseText);
+            }
+            else {
+                reject(xHttp.statusText);
+            }
+        };
+        xHttp.open("GET", 'https://api.github.com/search/repositories?q=' + keyword);
+        xHttp.send();
+    });
+    promiseObj.then(value => {
+        console.log("OK");
+        if (typeof value === "string") {
+            document.getElementById('display').innerHTML = value;
+            0;
+        }
+        console.log("End");
+    }, reason => {
+        console.log("NG");
+        console.log(reason);
+    });
+}
